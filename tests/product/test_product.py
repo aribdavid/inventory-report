9f898a2940abaa258101d7ef665b60b0a96e1ae9
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-09-28 11:23:26 -0300

    created test product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
-def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+def mock_products():
+    return Product(
+        id=123,
+        nome_do_produto="nome_do_produto",
+        nome_da_empresa="nome_da_empresa",
+        data_de_fabricacao="data_de_fabricacao",
+        data_de_validade="data_de_validade",
+        numero_de_serie="numero_de_serie",
+        instrucoes_de_armazenamento="instrucoes_de_armazenamento",
+    )
+
+
+def test_cria_produto():    
+    product = mock_products()
+    assert product.id == 123
+    assert product.nome_do_produto == 'nome_do_produto'
+    assert product.nome_da_empresa == 'nome_da_empresa'
+    assert product.data_de_fabricacao == 'data_de_fabricacao'
+    assert product.data_de_validade == 'data_de_validade'
+    assert product.numero_de_serie == 'numero_de_serie'
+    assert product.instrucoes_de_armazenamento == 'instrucoes_de_armazenamento'
