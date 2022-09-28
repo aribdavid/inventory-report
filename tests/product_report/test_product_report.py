9b53a58e1963b187634236980137f9b63583b51a
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-09-28 11:26:03 -0300

    created test product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+
+def mock_products():
+    return Product(
+        id=123,
+        nome_do_produto="nome_do_produto",
+        nome_da_empresa="nome_da_empresa",
+        data_de_fabricacao="data_de_fabricacao",
+        data_de_validade="data_de_validade",
+        numero_de_serie="numero_de_serie",
+        instrucoes_de_armazenamento="instrucoes_de_armazenamento",
+    )
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = mock_products()
+    stringRepr = str(product.__repr__)
+    text1 = "nome_do_produto"
+    text2 = "data_de_validade"
+    assert text1 in stringRepr
+    assert text2 in stringRepr
